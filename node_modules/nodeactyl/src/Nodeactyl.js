78a8006f21fc0bf77bdfdf4793f75b5f8b3c5d2d
MadhavRP <ilikegameslol8@gmail.com>
2022-11-26 21:17:21 +0530

    Removed node_modules folder

--- node_modules/nodeactyl/src/Nodeactyl.js
+++
@@ -1,9 +0,0 @@
-const Client = require('./client/Client.js');
-const Application = require('./application/Application.js');
-const ServerBuilder = require('./utils/ServerBuilder.js');
-
-module.exports = {
-    NodeactylClient: Client,
-    NodeactylApplication: Application,
-    ServerBuilder: ServerBuilder
-};
